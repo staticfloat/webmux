a2642fdc71dc629b7c90a9bd63deb89eb7f99722
Elliot Saba <staticfloat@gmail.com>
2018-03-03 21:30:11 +0000

    Add embedded SSH server to docker container!

---
+++ pyapp/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# When we get killed, kill all our children
+trap "exit" INT TERM
+trap "kill 0" EXIT
+
+# Start sshd in the background
+/usr/sbin/sshd
+
+# start our python app in the background
+python webmux.py &
+PYTHON_PID=$!
+wait $PYTHON_PID
